da51f07353d76119273f0ed37909094602bdbe8a
santushtisharma10 <sharmasantushti1012@gmail.com>
2022-02-28 11:54:07 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const PORT = 5000 
+const PORT = 5000 || process.env.PORT
+const dbUrl = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PWD}@cluster0.a4ouc.mongodb.net/Sapling?retryWrites=true&w=majority`
+
+mongoose.connect(dbUrl)
+.then(()=>console.log("database connected"))
 
 app.get("/",(req,res)=> {
 
     res.send("Welcome to Elite Hackers Backend")
+})
+app.post("/login", (req, res)=> {
+
 })
 
 app.listen(PORT,()=>console.log(`Listening to port ${PORT}`))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    
+  }
   return (
     <div className="App">
       Elite hackers
+      <form onSubmit={handleSubmit}>
+
+      </form>
     </div>
   );
 }
