f18872a868b271386b57aed5c74ad8793a434a7e
syedareehaquasar <reeha.saba@gmail.com>
2022-03-30 12:42:08 +0530

    added responsive dashboard

---
+++ dashboard/config/defaultSettings.js
@@ -0,0 +1,14 @@
+const Settings = {
+  navTheme: 'light',
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'Elite Hackers',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+export default Settings;
