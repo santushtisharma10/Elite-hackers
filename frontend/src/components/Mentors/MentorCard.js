f12488f84cf58ffeece3de7baecd928f1aa3d66b
Aastha2602 <aasthaggarwal2000@gmail.com>
2022-05-16 19:21:59 +0530

    updated

--- frontend/src/components/Mentors/MentorCard.js
+++ frontend/src/components/Mentors/MentorCard.js
@@ -11,19 +11,7 @@ import img8 from '../../Asset/d1.png';
 function MentorCard() {
   return (
     <div className="mentor_main">
-      {/* <!-- Navigation Bar --> */}
-	<header id="mentor_header">
-		<div class="mentor_head-container">
-			<div class="mentor_quote">
-				<p>The beautiful thing about learning is that nobody can take it away from you</p>
-				<h5>Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include teaching, training, storytelling, discussion and directed research!</h5>
-			</div>
-			<div class="mentor_svg-image">
-				<img src={img1} className="img-fluid animated" id="floating" alt="svg"></img>
-			</div>
-		</div>
-	</header>
-	
+
 	<div className='lower_main'>
 	{/* <!-- Some Popular Subjects --> */}
 	<div className="title">
@@ -40,18 +28,55 @@ function MentorCard() {
 	{/* <!-- SERVICES --> */}
 	<div class="service-swipe">
 		<div class="diffSection" id="services_section">
-		<center><p>Services</p></center>
 		</div>
 		<a href="/course"><div class="s-card"><img src={img3}></img><p>Online Courses</p></div></a>
 		<a href="/books"><div class="s-card"><img src={img4}></img><p>Popular books and resources</p></div></a>
 		<a href="/chat"><div class="s-card"><img src={img5}></img><p>Discussion with Mentors</p></div></a>
-		<a href="/"><div class="s-card"><img src={img6}></img><p>Blogs</p></div></a>
 	</div>
 	<br />
 	<br />
 	</div>
 
-	<marquee direction="left" onmouseover="this.stop()" onmouseout="this.start()" scrollamount="20"><div class="marqu">“Education is the passport to the future, for tomorrow belongs to those who prepare for it today.” “Your attitude, not your aptitude, will determine your altitude.” “If you think education is expensive, try ignorance.” “The only person who is educated is the one who has learned how to learn …and change.”</div></marquee>
+      {/* <!-- Navigation Bar --> */}
+	<header id="mentor_header">
+		<div class="mentor_head-container">
+			<div class="mentor_quote">
+				<p>The beautiful thing about learning is that nobody can take it away from you</p>
+				<h5>Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include teaching, training, storytelling, discussion and directed research!</h5>
+			</div>
+			<div class="mentor_svg-image">
+				<img src={img1} className="img-fluid animated" id="floating" alt="svg"></img>
+			</div>
+		</div>
+	</header>
+	
+	{/* <div className='lower_main'>
+	{/* <!-- Some Popular Subjects --> */}
+	{/* <div className="title">
+		<span>Enhance your skills</span>
+	</div>
+	<div className="course">
+		<center><div class="cbox">
+		<div class="det"><img src={img7}></img>Self Paced Learning</div>
+		<div class="det"><img src={img8}></img>Mentoship Support</div>
+		</div></center>
+	</div> */} 
+
+
+	{/* <!-- SERVICES --> */}
+	{/* <div class="service-swipe">
+		<div class="diffSection" id="services_section">
+		<center><p>Services</p></center>
+		</div>
+		<a href="/course"><div class="s-card"><img src={img3}></img><p>Online Courses</p></div></a>
+		<a href="/books"><div class="s-card"><img src={img4}></img><p>Popular books and resources</p></div></a>
+		<a href="/chat"><div class="s-card"><img src={img5}></img><p>Discussion with Mentors</p></div></a>
+	</div>
+	<br />
+	<br />
+	</div> */}
+
+	<marquee direction="left" onmouseover="this.stop()" onmouseout="this.start()" scrollamount="5"><div class="marqu">“Education is the passport to the future, for tomorrow belongs to those who prepare for it today.” “Your attitude, not your aptitude, will determine your altitude.” “If you think education is expensive, try ignorance.” “The only person who is educated is the one who has learned how to learn …and change.”</div></marquee>
     </div>
   )
 }
